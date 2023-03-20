2ea5d2693e821d298ead1d57438f6ad4bb9665d9
TomB201 <tom.brchn@eduge.ch>
2023-03-20 14:14:34 +0100

    busschedule

---
+++ android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -0,0 +1,57 @@
+package com.example.inventory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.inventory.data.Item
+import com.example.inventory.data.ItemDao
+import kotlinx.coroutines.launch
+import androidx.lifecycle.viewModelScope
+
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+
+    private fun insertItem(item: Item) {
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
+    }
+
+    fun isEntryValid(itemName: String, itemPrice: String, itemCount: String): Boolean {
+        if (itemName.isBlank() || itemPrice.isBlank() || itemCount.isBlank()) {
+            return false
+        }
+        return true
+    }
+
+    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item {
+        return Item(
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+
+    fun addNewItem(itemName: String, itemPrice: String, itemCount: String) {
+        val newItem = getNewItemEntry(itemName, itemPrice, itemCount)
+        insertItem(newItem)
+    }
+}
+
+fun isEntryValid(itemName: String, itemPrice: String, itemCount: String): Boolean {
+    if (itemName.isBlank() || itemPrice.isBlank() || itemCount.isBlank()) {
+        return false
+    }
+    return true
+}
+
+class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
+        @Suppress("UNCHECKED_CAST")
+        return InventoryViewModel(itemDao) as T
+    }
+        throw IllegalArgumentException("Unknown ViewModel class")
+
+        TODO("Not yet implemented")
+
+    }
+
+}
